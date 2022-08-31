1a01609fd0473fc7201eb9491667e02f06fe3ec0
jovimarque <74689585+jovimarque@users.noreply.github.com>
2022-08-31 09:51:11 -0300

    Update usuario.php
    Foi inserido novos metodos

--- class/usuario.php
+++ class/usuario.php
@@ -63,6 +63,45 @@ class Usuario {
 		}
 	}
 
+	
+	// essa função carrega estática carrega todos os usuários
+	public  static function getList(){
+		$sql = new Sql();
+	return 	$sql->select("SELECT * FROM tb_usuario ORDER BY deslogin");
+	}
+
+	//essa  função estática carrega a lista de login pelo nome 
+	public static function search($login){
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuario WHERE deslogin LIKE :SEARCH ORDER BY deslogin ", array('SEARCH'=>"%".$login."%"));
+	}
+
+
+	//essa função carrega o usuario após validar login e senha
+	public function login($usario, $senha){
+		$sql = new Sql();
+		$result = $sql->select("SELECT * FROM tb_usuario WHERE deslogin = :LOGIN AND dessenha = :SENHA" , array(
+			":LOGIN"=> $usario,
+			":SENHA"=>$senha
+
+	)); 
+
+		if(count($result) > 1){
+
+			$row = $result[0];
+			$this->setIdUsuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro($row['dtcadastro']);
+
+		}else{
+
+			throw new Exception("Login ou senha inválidos");
+		}
+
+	
+	
 	public function __toString(){
 			return json_encode(array("idusuario"=>$this->getIdUsuario(),
 				"deslogin"=>$this->getDeslogin(),
